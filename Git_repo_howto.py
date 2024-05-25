6fa21e25f4efa97da0159a33c391cae40f92c0b9
ceubie <cras.ceubanks@gmail.com>
2024-05-25 16:13:53 -0400

    added code

--- Git_repo_howto.py
+++ Git_repo_howto.py
@@ -4,7 +4,7 @@
 
 
 # Answer: 
-# One, open the folder that you would like to use as your repository
+# One, open your IDE. Then open the folder that you would like to use as your repository
 # Two, initialize the repository using "git init" in a new terminal
 # Three, add the files you would like in the repository using "git add"
             #note: "git add ."" will include everything in the folder
