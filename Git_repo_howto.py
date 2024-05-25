e37026940aabe810a8fbf0ef69d082335172de15
ceubie <cras.ceubanks@gmail.com>
2024-05-25 15:58:41 -0400

    first commit

---
+++ Git_repo_howto.py
@@ -0,0 +1,12 @@
+# Question: 
+# How do you start a git repository?
+
+
+
+# Answer: 
+# First, open the folder that you would like to use as your repository
+# Second, initialize the repository using "git init" in a new terminal
+# Third, add the files you would like in the repository using "git add"
+            #note: "git add ."" will include everything in the folder
+# Fourth, commit the repo - "git commit" 
+            #note: " -m" to create message
